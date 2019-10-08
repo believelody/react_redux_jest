5e43e118ec80cac8c09bd51b9d9cbd2150d4f563
Believe LODY <blody@adneom.com>
2019-10-08 14:57:41 +0200

    designed header and logo component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.scss'
+import Logo from '../logo/Logo'
+
+const Header = () => {
+    return (
+        <header>
+            Header
+            <div className="wrap">
+                <Logo />
+            </div>
+        </header>
+    )
+}
+
+export default Header
